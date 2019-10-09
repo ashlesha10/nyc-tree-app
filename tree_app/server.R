fe9db472dc79d6a37c4708d6f9dbc9af231daa53
cw3105 <55252774+cw3105@users.noreply.github.com>
2019-10-08 20:55:10 -0400

    Update server.R

--- tree_app/server.R
+++ tree_app/server.R
@@ -12,6 +12,7 @@ library(leaflet)
 library(htmltools)
 library(ggplot2)
 library(dplyr)
+library(ggpubr)
 
 load('../output/boroughs.RData')
 load('../output/borough_problems.RData')
@@ -22,8 +23,65 @@ dog_count <- dogs %>%
   summarise(countbyzip = n())
 dogs <- dogs %>%
   left_join(dog_count, by = c('ZipCode','borough'))
+dogs.count <- dog_count[,2:3]
+names(dogs.count) <- c("ZipCode", "Dogs")
 
 shinyServer(function(input, output, session) {
+  
+  output$plot2 <- renderPlot({
+    trees.dead <- trees[which(trees$status == "Dead"),]
+    trees.dead$postcode <- as.character(trees.dead$postcode)
+    trees.count <- trees.dead %>%
+      group_by(postcode) %>% 
+      summarise(countbyzip = n())
+    names(trees.count) <- c("ZipCode", "Dead Trees")
+    dogs.trees <- merge(data.frame(dogs.count, row.names=NULL), data.frame(trees.count, row.names=NULL), by = "ZipCode", all = FALSE)
+    ggscatter(dogs.trees, x = "Dogs", y = "Dead.Trees", 
+              add = "reg.line", conf.int = TRUE, 
+              cor.coef = TRUE, cor.method = "pearson", title = "Dogs vs Trees",
+              xlab = "Number of Dogs", ylab = "Number of Dead Trees")
+  })
+  output$plot3 <- renderPlot({
+    trees.poor <- trees[which(trees$health == "Poor"),]
+    trees.poor$postcode <- as.character(trees.poor$postcode)
+    treespoor.count <- trees.poor %>%
+      group_by(postcode) %>% 
+      summarise(countbyzip = n())
+    names(treespoor.count) <- c("ZipCode", "Poor Trees")
+    dogs.poor <- merge(data.frame(dogs.count, row.names=NULL), data.frame(treespoor.count, row.names=NULL), by = "ZipCode", all = FALSE)
+    ggscatter(dogs.poor, x = "Dogs", y = "Poor.Trees", 
+              add = "reg.line", conf.int = TRUE, 
+              cor.coef = TRUE, cor.method = "pearson", title = "Dogs vs Poor Health Trees", 
+              xlab = "Number of Dogs", ylab = "Number of Poor Health Trees") 
+  })
+  output$plot4 <- renderPlot({
+    trees.root <- trees[which(trees$root_other == "Yes"),]
+    trees.root$postcode <- as.character(trees.root$postcode)
+    treesroot.count <- trees.root %>%
+      group_by(postcode) %>% 
+      summarise(countbyzip = n())
+    names(treesroot.count) <- c("ZipCode", "Root Problem")
+    dogs.root <- merge(data.frame(dogs.count, row.names=NULL), data.frame(treesroot.count, row.names=NULL), by = "ZipCode", all = FALSE)
+    ggscatter(dogs.root, x = "Dogs", y = "Root.Problem", 
+              add = "reg.line", conf.int = TRUE, 
+              cor.coef = TRUE, cor.method = "pearson",
+              xlab = "Number of Dogs", ylab = "Number of Trees with other Root Problems") +
+      labs(title = "Dogs vs Trees \nwith Other Root Problems")
+  })
+  output$plot5 <- renderPlot({
+    trees.trunk <- trees[which(trees$trnk_other == "Yes"),]
+    trees.trunk$postcode <- as.character(trees.trunk$postcode)
+    treestrunk.count <- trees.trunk %>%
+      group_by(postcode) %>% 
+      summarise(countbyzip = n())
+    names(treestrunk.count) <- c("ZipCode", "Trunk Problem")
+    dogs.trunk <- merge(data.frame(dogs.count, row.names=NULL), data.frame(treestrunk.count, row.names=NULL), by = "ZipCode", all = FALSE)
+    ggscatter(dogs.trunk, x = "Dogs", y = "Trunk.Problem", 
+              add = "reg.line", conf.int = TRUE, 
+              cor.coef = TRUE, cor.method = "pearson", 
+              xlab = "Number of Dogs", ylab = "Number of Trees with other Trunk Problems") + 
+      labs(title = "Dogs vs Trees \nwith other Trunk Problems")
+  })
   
   # Reactive expression for the data subsetted to what the user selected
   tree_filteredData <- reactive({
